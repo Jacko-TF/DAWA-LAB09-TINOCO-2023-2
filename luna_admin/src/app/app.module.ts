728528de15269c3fabd228fefd82950d6499597c
Jacko <jacko.tinoco@tecsup.edu.pe>
2023-10-31 19:53:44 -0500

    pages

--- luna_admin/src/app/app.module.ts
+++ luna_admin/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductosComponent } from './productos/productos.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './pages/header/header.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent
+    ProductosComponent,
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
